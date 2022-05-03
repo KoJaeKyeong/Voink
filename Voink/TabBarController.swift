995d2dd6e7a9e73056faa5cc3d9f7d73e6f9c12a
고재경 <folw159@gmail.com>
2022-05-03 14:40:02 +0900

    remove Viewcontroller and Add MapViewController, RecordViewController, RecordListViewController

---
+++ Voink/TabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarController.swift
+//  Voink
+//
+//  Created by Jae Kyeong Ko on 2022/05/03.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
