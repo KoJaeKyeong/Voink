9bd0a06d276685d7a4154892fa9ee1c4c8385206
고재경 <folw159@gmail.com>
2022-05-29 17:14:18 +0900

    update .gitignore

---
+++ Voink/UI/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  Voink
+//
+//  Created by Jae Kyeong Ko on 2022/05/29.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
