f8ee593f1baeaf06ab9fd33a9d8bb1d48d2c91a8
고재경 <folw159@gmail.com>
2022-05-29 16:18:39 +0900

    update Storage to KeyStorage

--- Voink/AppDelegate.swift
+++ Voink/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        GMSServices.provideAPIKey(Storage().googleMapsAPIKey)
+        GMSServices.provideAPIKey(KeyStorage().googleMapsAPIKey)
         FirebaseApp.configure()
         return true
     }
---
+++ Voink/KeyStorage.swift
@@ -0,0 +1,10 @@
+//
+//  KeyStorage.swift
+//  Voink
+//
+//  Created by Jae Kyeong Ko on 2022/05/29.
+//
+
+struct KeyStorage {
+    let googleMapsAPIKey = "AIzaSyD-wyR1l6apTrRuhzI3dOp_CwB7ty0UbpY"
+}
